171cbd5bdb71f46ac8a539acaa085add3042379b
user <hag.hispin12@gmail.com>
2024-12-10 15:12:32 +0200

    version10

--- app/src/main/java/com/example/mathprojectdavid/MyFruitsAdapter.java
+++ app/src/main/java/com/example/mathprojectdavid/MyFruitsAdapter.java
@@ -1,24 +1,41 @@
-package com.example.mathprojectdavid;
-
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
-
-public class MyFruitsAdapter extends RecyclerView .Adapter<MyFruitsAdapter.MyViewHolder>{
-    public static class MyViewHolder extends RecyclerView.ViewHolder{
-        TextView tvFruitName;
-        ImageView ivFruitImg;
-
-        public MyViewHolder(@NonNull View itemView){
-            super(itemView);
-            tvFruitName = itemView.findViewById(R.id.tvFruitName);
-            ivFruitImg = itemView.findViewById(R.id.ivFruitImg);
-        }
-        public void bind(final User item, final OneItemClickListener listener){
-            ivFruitImg.setText()
-        }
-    }
-}
+//package com.example.mathprojectdavid;
+//
+//import android.view.View;
+//import android.widget.ImageView;
+//import android.widget.TextView;
+//
+//import androidx.annotation.NonNull;
+//import androidx.recyclerview.widget.RecyclerView;
+//
+//import java.util.ArrayList;
+//
+//public class MyFruitsAdapter extends RecyclerView .Adapter<MyFruitsAdapter.MyViewHolder>{
+//
+//    public interface OnItemClickListener{
+//        void onItemClick(Fruit item);
+//    }
+//
+//    private ArrayList<Fruit> fruits;
+//    private OnItemClickListener
+//
+//    public static class MyViewHolder extends RecyclerView.ViewHolder{
+//        TextView tvFruitName;
+//        ImageView ivFruitImg;
+//
+//        public MyViewHolder(@NonNull View itemView){
+//            super(itemView);
+//            tvFruitName = itemView.findViewById(R.id.tvFruitName);
+//            ivFruitImg = itemView.findViewById(R.id.ivFruitImg);
+//        }
+//        public void bind(final User item, final OneItemClickListener listener){
+//            tvFruitName.setText(item.getName());
+//            ivFruitImg.setImageResource(item.getDrawble());
+//            itemView.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    listener.onItemClick(item);
+//                }
+//            });
+//        }
+//    }
+//}
--- app/src/main/java/com/example/mathprojectdavid/User.java
+++ app/src/main/java/com/example/mathprojectdavid/User.java
@@ -1,9 +1,29 @@
 package com.example.mathprojectdavid;
 
+import android.net.Uri;
+
 public class User {
     private String name;
     private int score;
     private int rate;
+    private Long id;
+    Uri uri;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Uri getUri() {
+        return uri;
+    }
+
+    public void setUri(Uri uri) {
+        this.uri = uri;
+    }
 
     public String getName() {
         return name;
