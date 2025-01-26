ad4b799c0bcd18568bf303fe13dc2add18328107
user <hag.hispin12@gmail.com>
2025-01-26 15:29:39 +0200

    סיימתי ספינר

--- app/src/main/java/BugrutProject/LoginActivity.java
+++ app/src/main/java/BugrutProject/LoginActivity.java
@@ -1,9 +1,13 @@
 package BugrutProject;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -21,6 +25,14 @@ public class LoginActivity extends AppCompatActivity {
 
     Spinner spinerLogin;
 
+    private EditText Username;
+
+    private EditText password;
+
+    private Button Login;
+
+    Intent intent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,7 +47,8 @@ public class LoginActivity extends AppCompatActivity {
         spinerLogin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
-                String item = adapterView.getItemIdAtPosition(position)+"";
+                String item = adapterView.getItemAtPosition(position).toString();
+                //String item = adapterView.getItemIdAtPosition(position)+"";
                 Toast.makeText(LoginActivity.this, "בחר סוג כניסה" + item, Toast.LENGTH_SHORT).show();
             }
 
@@ -45,10 +58,32 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
         creatListOfLogin();
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref", MODE_PRIVATE);
+                SharedPreferences.Editor myEdit = sharedPreferences.edit();
+                myEdit.putString("name", Username.getText().toString());
+                myEdit.putString("password", password.getText().toString());
+                myEdit.apply();
+                intent = new Intent(LoginActivity.this, MainActivity.class);
+                intent.putExtra("UserName", Username.getText().toString());
+                intent.putExtra("password", password.getText().toString());
+                startActivity(intent);
+            }
+        });
+        SharedPreferences sh = getSharedPreferences("MySharedPref", MODE_PRIVATE);
+        String s1 = sh.getString("name", "");
+        String s2 = sh.getString("password", "");
+        Username.setText(s1);
+        password.setText(s2);
     }
 
     public void initView(){
         spinerLogin = findViewById(R.id.spinerLogin);
+        Username = findViewById(R.id.UserNameLogin);
+        password = findViewById(R.id.passwordLogin);
+        Login = findViewById(R.id.submitLogin);
     }
 
     public void creatListOfLogin(){
